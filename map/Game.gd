7d693f0bb384aa2e9686bf71ea3e454a8b943720
Moon <43276017+GhostWalker562@users.noreply.github.com>
2020-07-12 00:12:56 -0700

    Major Commit
    Decorations

--- map/Game.gd
+++ map/Game.gd
@@ -1,9 +1,11 @@
+
 extends Navigation2D
 
 
 func _process(delta):
-	if(get_children().size() ==4):
+	if(get_children().size() == 5):
 		get_node("Player")._play_win();
 		set_process(false)
 		yield(get_tree().create_timer(7),"timeout")
 		get_tree().quit()
+		
